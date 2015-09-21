ed8326b141a849bc229670737da0413d856342b5
Pat Viafore <patviafore@gmail.com>
2015-09-21 18:25:30 -0500

    Adding flag index checking to board

--- battleline/model/Board.py
+++ battleline/model/Board.py
@@ -4,5 +4,22 @@ class Board(object):
     A board is represented as 9 flags, each with 3 potential slots on the top and bottom
     """
 
+    def __is_flag_in_range(self,index):
+        return index in range(1,11)
+
     def get_flag(self, flag_index):
+        """Get the Flag associated with the index"""
+        if not self.__is_flag_in_range(flag_index):
+            raise FlagNotFoundError(flag_index)
+
         return Flag()
+
+class FlagNotFoundError(Exception):
+    def __init__(self, flag_index):
+        """Create an Exception that the index is not valid
+        @param flag_index the index of the flag that was not valid
+        """
+        self.index = flag_index
+
+    def __str__(self):
+        return "Flag Index {} is invalid".format(self.index)
--- test/model/testBoard.py
+++ test/model/testBoard.py
@@ -1,7 +1,14 @@
 import unittest
-from battleline.model.Board import Board
+from battleline.model.Board import Board, FlagNotFoundError
 class TestBoard(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+
     def test_board_is_empty_to_begin_with(self):
-        board = Board()
         for flag in xrange(1,11):
-            self.assertTrue(board.get_flag(flag).is_empty())
+            self.assertTrue(self.board.get_flag(flag).is_empty())
+
+    def test_board_cannot_get_invalid_flags(self):
+        self.assertRaisesRegexp(FlagNotFoundError, "Flag Index 0 is invalid", self.board.get_flag, 0)
+        self.assertRaisesRegexp(FlagNotFoundError, "Flag Index 11 is invalid", self.board.get_flag, 11)
