932f2f8fc036a7bce285855a73948b7b485a2a51
Pat Viafore <patviafore@gmail.com>
2015-09-21 19:41:46 -0500

    Only have 9 flags instead of 10 Treat All 9 flags as distinct flags in the board

--- test/model/testBoard.py
+++ test/model/testBoard.py
@@ -6,9 +6,16 @@ class TestBoard(unittest.TestCase):
         self.board = Board()
 
     def test_board_is_empty_to_begin_with(self):
-        for flag in xrange(1,11):
+        for flag in xrange(1,10):
             self.assertTrue(self.board.get_flag(flag).is_empty())
 
     def test_board_cannot_get_invalid_flags(self):
         self.assertRaisesRegexp(FlagNotFoundError, "Flag Index 0 is invalid", self.board.get_flag, 0)
-        self.assertRaisesRegexp(FlagNotFoundError, "Flag Index 11 is invalid", self.board.get_flag, 11)
+        self.assertRaisesRegexp(FlagNotFoundError, "Flag Index 10 is invalid", self.board.get_flag, 10)
+
+    def test_board_has_nine_distinct_flags(self):
+        board_flags = [self.board.get_flag(x) for x in xrange(1,10)]
+        self.assertEquals(9, len(set(board_flags)))
+
+    def test_getting_a_flag_at_one_index_is_repeatable(self):
+        self.assertEquals(self.board.get_flag(1), self.board.get_flag(1))
