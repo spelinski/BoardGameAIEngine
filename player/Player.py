b201769614f5ed1976d5dbda32c0abacd1754c4b
Pat Viafore <patviafore@gmail.com>
2015-09-22 19:18:45 -0500

    Adding not implemented error to take_turn

--- player/Player.py
+++ player/Player.py
@@ -6,3 +6,6 @@ class Player(object):
         @param name name of the player
         """
         self.name = name
+
+    def take_turn(self):
+        raise NotImplementedError
--- test/player/testPlayer.py
+++ test/player/testPlayer.py
@@ -5,3 +5,7 @@ class TestPlayer(unittest.TestCase):
     def test_player_can_be_given_a_name(self):
         player = Player("Player 1")
         self.assertEquals("Player 1", player.name)
+
+    def test_take_turn_is_unimplemented(self):
+        with self.assertRaises(NotImplementedError):
+            Player("").take_turn()
