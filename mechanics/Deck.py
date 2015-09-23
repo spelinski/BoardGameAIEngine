27cb9f272507c3cfc04747b9f35c2566e042f263
Spelinski <rohk@hotmail.com>
2015-09-22 23:01:33 -0500

    PEP8 my code except for a over 80 column line in the test file

--- mechanics/Deck.py
+++ mechanics/Deck.py
@@ -1,8 +1,11 @@
 from random import shuffle
+
+
 class Deck(object):
     """
     Deck object
     """
+
     def __init__(self, listOfCards, shuffleDeck=True):
         """
         Constructor
@@ -14,15 +17,15 @@ class Deck(object):
         self.deck = listOfCards
         if shuffleDeck:
             self.shuffle()
-            
+
     def shuffle(self):
         shuffle(self.deck)
-        
+
     def is_empty(self):
         if len(self.deck) > 0:
             return False
         return True
-    
+
     def draw(self):
         """
         draw the next card from the deck
@@ -31,13 +34,15 @@ class Deck(object):
         if not self.is_empty():
             return self.deck.pop()
         raise DeckEmptyError("draw")
-    
+
+
 class DeckEmptyError(Exception):
+
     def __init__(self, attemptedAction):
         """Create an Exception that the index is not valid
         @param attemptedAction what was trying to be done on an empty deck
         """
         self.attemptedAction = attemptedAction
-        
+
     def __str__(self):
         return "Attempted to {} on an empty Deck".format(self.attemptedAction)
--- test/mechanics/testDeck.py
+++ test/mechanics/testDeck.py
@@ -3,40 +3,40 @@ from mechanics.Deck import Deck
 from mechanics.Deck import DeckEmptyError
 from collections import namedtuple
 
+
 class TestDeck(unittest.TestCase):
+
     def test_deck_is_not_empty_by_default(self):
         Card = namedtuple('Card', 'number,color')
-        listOfCards = [Card(number=1,color="Blue")]
+        listOfCards = [Card(number=1, color="Blue")]
         self.assertFalse(Deck(listOfCards).is_empty())
-        
+
     def test_can_draw_card_from_deck(self):
         Card = namedtuple('Card', 'number,color')
-        listOfCards = [Card(number=1,color="Blue")]
+        listOfCards = [Card(number=1, color="Blue")]
         drawnCard = Deck(listOfCards).draw()
-        self.assertGreater(drawnCard.number,0)
-        self.assertGreater(len(drawnCard.color), 0)        
-        
+        self.assertGreater(drawnCard.number, 0)
+        self.assertGreater(len(drawnCard.color), 0)
+
     def test_can_draw_different_cards_from_deck(self):
         Card = namedtuple('Card', 'number,color')
-        listOfCards = [Card(number=1,color="Blue"),Card(number=2,color="Blue")]
+        listOfCards = [Card(number=1, color="Blue"),
+                       Card(number=2, color="Blue")]
         localDeck = Deck(listOfCards)
         self.assertNotEqual(localDeck.draw(), localDeck.draw())
-        
+
     def test_deck_is_empty_after_drawing_all_cards(self):
         Card = namedtuple('Card', 'number,color')
-        listOfCards = [Card(number=1,color="Blue"),Card(number=2,color="Blue")]
+        listOfCards = [Card(number=1, color="Blue"),
+                       Card(number=2, color="Blue")]
         localDeck = Deck(listOfCards)
         localDeck.draw()
         localDeck.draw()
         self.assertTrue(localDeck.is_empty())
-        
+
     def test_error_thrown_when_drawing_from_empty_deck(self):
         Card = namedtuple('Card', 'number,color')
-        listOfCards = [Card(number=1,color="Blue")]
+        listOfCards = [Card(number=1, color="Blue")]
         localDeck = Deck(listOfCards)
         localDeck.draw()
         self.assertRaisesRegexp(DeckEmptyError, "Attempted to draw on an empty Deck", localDeck.draw)
-            
-        
-        
-    
