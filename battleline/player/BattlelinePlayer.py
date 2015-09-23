88cb8dc33663b4d9e495ead803af3f7e10260d80
Pat Viafore <patviafore@gmail.com>
2015-09-22 19:45:39 -0500

    Added hand management to Battleline Player

---
+++ battleline/player/BattlelinePlayer.py
@@ -0,0 +1,34 @@
+from player.Player import Player
+class BattlelinePlayer(Player):
+    """
+    Player object for Battleline
+    Keeps track of a hand
+    """
+
+    HAND_LIMIT = 7
+
+    def __init__(self, name):
+        Player.__init__(self, name)
+        self.hand = []
+
+    def add_to_hand(self, card):
+        if(self.__is_hand_at_limit()):
+            raise HandFullError(BattlelinePlayer.HAND_LIMIT)
+        self.hand.append(card)
+
+    def __is_hand_at_limit(self):
+        return len(self.hand) == BattlelinePlayer.HAND_LIMIT
+
+class HandFullError(Exception):
+    def __init__(self, hand_limit):
+        """
+        Construtor
+        @param hand_limit the limit that the player hand can be
+        """
+        self.hand_limit = hand_limit
+
+    def __str__(self):
+        """
+        Return a string representation of the exception
+        """
+        return "Cannot exceed hand limit of {}".format(self.hand_limit)
