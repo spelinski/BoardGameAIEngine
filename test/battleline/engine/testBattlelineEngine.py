d8942b11a7e739ba09280851e6aa88dec14b8a5c
Pat Viafore <patviafore@gmail.com>
2015-09-22 21:46:37 -0500

    Adding intial engine code

--- test/battleline/engine/testBattlelineEngine.py
+++ test/battleline/engine/testBattlelineEngine.py
@@ -3,7 +3,17 @@ from battleline.engine.BattlelineEngine import BattlelineEngine
 from battleline.player.BattlelinePlayer import BattlelinePlayer
 class TestBattlelineEngine(unittest.TestCase):
 
+    def setUp(self):
+        self.engine = BattlelineEngine(BattlelinePlayer("1"), BattlelinePlayer("2"))
+
     def test_can_create_engine_with_two_players(self):
-        engine = BattlelineEngine(BattlelinePlayer("1"), BattlelinePlayer("2"))
-        self.assertEquals("1", engine.player1.name)
-        self.assertEquals("2", engine.player2.name)
+        self.assertEquals("1", self.engine.player1.name)
+        self.assertEquals("2", self.engine.player2.name)
+
+    def test_board_deck_should_have_all_sixty_cards_to_start_with(self):
+        #add a test in here once we have a deck working
+        pass
+
+    def test_each_player_starts_with_7_cards(self):
+        self.assertEquals(7, len(self.engine.player1.hand))
+        self.assertEquals(7, len(self.engine.player2.hand))
