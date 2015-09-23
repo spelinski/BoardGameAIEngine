0e04e320451b7729d8b4ebcbdf14016b15af7a10
Don Pollitz <chaos2007@gmail.com>
2015-09-22 23:11:39 -0500

    Adding minimal amount needed for checking flag constraints

--- battleline/model/Flag.py
+++ battleline/model/Flag.py
@@ -1,7 +1,65 @@
+
+
+class Card(object):
+    def __init__(self):
+        pass
+
+
 class Flag(object):
     """
     A representation of a flag
     """
+    PLAYER_ONE = "player1"
+    PLAYER_TWO = "player2"
+    MAX_CARDS_PER_SIDE = 3
 
-    def is_empty(self):
-        return True
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.sides = {self.PLAYER_ONE:[], self.PLAYER_TWO:[]}
+
+    def is_empty(self, player=None):
+        """Get the Flag associated with the index
+
+        @param player player constant to check, None will return empty status for either side of flag
+        """
+        if player == None:
+            return self.is_empty(self.PLAYER_ONE) and self.is_empty(self.PLAYER_TWO)
+        elif player not in self.sides:
+            raise InvalidPlayerError(player)
+        else:
+            return len(self.sides[player]) == 0
+
+    def add_card(self, player, card):
+        """Add a card to the Flag
+
+        @param player player side to add the flag to. 
+        @param card   the card to add to the player side
+        """
+        if player != self.PLAYER_ONE and player != self.PLAYER_TWO:
+            raise InvalidPlayerError(player)
+        if len(self.sides[player]) >= self.MAX_CARDS_PER_SIDE:
+            raise TooManyCardsOnOneSideError(player) 
+        self.sides[player].append(card)
+
+class InvalidPlayerError(Exception):
+     def __init__(self, player_string):
+         """Create an Exception that the player is not valid
+         @param player_string the player name that was not valid
+         """
+         self.player = player_string 
+ 
+     def __str__(self):
+         return "Player String {} is invalid".format(self.player)
+
+class TooManyCardsOnOneSideError(Exception):
+     def __init__(self, player_string):
+         """Create an Exception that the player is trying to add to many cards
+         @param player_string the player name that was adding too many cards 
+         """
+         self.player = player_string 
+ 
+     def __str__(self):
+         return "Player {} is attempting to add to many cards".format(self.player)
+
--- test/battleline/model/testFlag.py
+++ test/battleline/model/testFlag.py
@@ -1,6 +1,29 @@
 import unittest
-from battleline.model.Flag import Flag
+from battleline.model.Flag import Flag, InvalidPlayerError, Card, TooManyCardsOnOneSideError
 
 class TestFlag(unittest.TestCase):
+    def setUp(self):
+        self.flag = Flag()
+
     def test_flag_is_empty_by_default(self):
-        self.assertTrue(Flag().is_empty())
+        self.assertTrue(self.flag.is_empty())
+
+    def test_flag_is_empty_by_player(self):
+        self.assertTrue(self.flag.is_empty(Flag.PLAYER_ONE))
+        self.assertTrue(self.flag.is_empty(Flag.PLAYER_TWO))
+
+    def test_board_cannot_get_invalid_flags(self):
+        self.assertRaisesRegexp(InvalidPlayerError, "Player String player3 is invalid", self.flag.is_empty, "player3")
+
+    def test_flag_cannot_add_to_invalid_player(self):
+        self.assertRaisesRegexp(InvalidPlayerError, "Player String player3 is invalid", self.flag.add_card, "player3", Card())
+
+    def test_flag_player_and_global_not_empty_after_adding_card(self):
+        self.flag.add_card(Flag.PLAYER_ONE, Card())
+        self.assertFalse(self.flag.is_empty(Flag.PLAYER_ONE))
+        self.assertTrue(self.flag.is_empty(Flag.PLAYER_TWO))
+        self.assertFalse(self.flag.is_empty())
+
+    def test_flag_can_not_add_more_than_tree_cards(self):
+        for x in range(0,3): self.flag.add_card(Flag.PLAYER_ONE, Card())
+        self.assertRaisesRegexp(TooManyCardsOnOneSideError, "Player player1 is attempting to add to many cards", self.flag.add_card, "player1", Card())
