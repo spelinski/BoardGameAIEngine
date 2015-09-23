088a0d737439cd97e720d329cb73abc07c1d5a25
Pat Viafore <patviafore@gmail.com>
2015-09-22 22:18:08 -0500

    Adding stub communication for initial hand communication

--- battleline/engine/BattlelineEngine.py
+++ battleline/engine/BattlelineEngine.py
@@ -12,3 +12,5 @@ class BattlelineEngine(object):
         for i in xrange(7):
             self.player1.add_to_hand(self.deck.draw())
             self.player2.add_to_hand(self.deck.draw())
+        player1.communication.send_message("player 1 hand red 1 blue 5 white 2 green 10 red 3 green 3 white 6")
+        player2.communication.send_message("player 2 hand red 7 blue 4 white 8 green 1 red 2 green 4 white 9")
