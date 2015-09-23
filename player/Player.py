51238ad97176ef0ce72fc8bca8637c82f559d2d8
Pat Viafore <patviafore@gmail.com>
2015-09-22 19:10:47 -0500

    Adding Player name

---
+++ player/Player.py
@@ -0,0 +1,8 @@
+class Player(object):
+
+    def __init__(self, name):
+        """
+        Constructor
+        @param name name of the player
+        """
+        self.name = name
---
+++ test/player/testPlayer.py
@@ -0,0 +1,7 @@
+import unittest
+from player.Player import Player
+class TestPlayer(unittest.TestCase):
+
+    def test_player_can_be_given_a_name(self):
+        player = Player("Player 1")
+        self.assertEquals("Player 1", player.name)
