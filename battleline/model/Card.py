b50a3f61424c30f67ab6504922e9dbaadc39b2d7
Spelinski <rohk@hotmail.com>
2015-09-22 21:22:59 -0500

    initiale structure of Deck

---
+++ battleline/model/Card.py
@@ -0,0 +1,10 @@
+class Card(object):
+    def __init__(self, number, color):
+        self.number = number
+        self.color = color
+    
+    def getNumber(self):
+        return self.number
+    
+    def getColor(self):
+        return self.color
