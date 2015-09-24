643e10122b7f2a0b8013297b4ffb11e9e5b656f5
Pat Viafore <patviafore@gmail.com>
2015-09-23 23:30:34 -0500

    Updating engine to use actual deck

--- battleline/engine/BattlelineEngine.py
+++ battleline/engine/BattlelineEngine.py
@@ -1,14 +1,35 @@
-from battleline.model.Deck import Deck
+from mechanics.Deck import Deck
+from collections import namedtuple
+from itertools import product
 
+TroopCard = namedtuple("TroopCard", ["color", "number"])
 class BattlelineEngine(object):
     """
     An engine that coordinates two players, a board and the decks for battleline
     """
 
     def __init__(self, player1, player2):
+        """
+        Constructor
+        @param player1 the first player
+        @param player2 the second player
+        """
         self.player1 = player1
         self.player2 = player2
-        self.deck = Deck()
+        self.troop_deck = Deck(self.get_troop_cards())
+
+    def initialize(self):
+        """
+        Initialize the game
+        """
         for i in xrange(7):
-            self.player1.add_to_hand(self.deck.draw())
-            self.player2.add_to_hand(self.deck.draw())
+            self.player1.add_to_hand(self.troop_deck.draw())
+            self.player2.add_to_hand(self.troop_deck.draw())
+
+    def get_troop_cards(self):
+        """
+        Get the troop cards
+        A list of all troop cards
+        """
+        colors = ["RED", "GREEN", "ORANGE", "YELLOW", "BLUE", "PURPLE"]
+        return [TroopCard(name,number) for name,number in sorted(product(colors, range(1,11)), reverse=True)]
