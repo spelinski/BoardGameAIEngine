ff331405a18359eacd92a19ae0963c96d46ffc02
Spelinski <rohk@hotmail.com>
2015-09-26 22:20:49 -0500

    fixed the pre-commit hooks for autopep8

--- communcation/PlayerCommunication.py
+++ communcation/PlayerCommunication.py
@@ -5,10 +5,13 @@ Created on Sep 23, 2015
 '''
 from subprocess import Popen, PIPE
 import threading
+
+
 class PlayerCommunication(object):
     """
     Class to communicate back and forth with an external program
     """
+
     def __init__(self, programWithPath):
         """
         Constructor
@@ -16,6 +19,7 @@ class PlayerCommunication(object):
         """
         self.runningPlayer = Popen(
             "./" + programWithPath, stdin=PIPE, stdout=PIPE, shell=True)
+
     def send_message(self, message):
         """
         send a string to the external program
@@ -23,6 +27,7 @@ class PlayerCommunication(object):
         """
         self.runningPlayer.stdin.write(message + "\n")
         self.runningPlayer.stdin.flush()
+
     def get_response(self, timeout=10):
         """
         get the response from the external program
@@ -39,6 +44,7 @@ class PlayerCommunication(object):
         if thread.is_alive():
             raise BotCommunicationError("timeout")
         return self.response
+
     def __get_response_thread(self):
         """
         get_response calls this so that it can run
