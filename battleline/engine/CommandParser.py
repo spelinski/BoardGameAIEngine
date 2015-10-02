8ab599e69c5755d57c75d6de4128a006e71821cd
Pat Viafore <patviafore@gmail.com>
2015-10-01 21:24:05 -0500

    Creating a client command parser

--- battleline/engine/CommandParser.py
+++ battleline/engine/CommandParser.py
@@ -3,8 +3,14 @@ class Identifiers(object):
     NORTH = "north"
     SOUTH = "south"
 
-class CommandParser(object):
+def make_dict(type, value):
+    return {"type": type, "value": value}
 
+
+class ServerCommandParser(object):
+    """
+    Meant for parsing messages that come from the server
+    """
     def parse(self, message):
         """
         Parse the mesage and return a dict of information
@@ -12,22 +18,36 @@ class CommandParser(object):
         @return a dictionary of {type: message type,  values : arbitrary}
         @raise InvalidParseError if the message was not of the type we expected
         """
-        if self.__is_player_name_request(message): return self.__parse_player_name_request(message)
+        if self.__is_player_name_request(message):
+            return self.__parse_player_name_request(message)
         raise InvalidParseError(message)
 
-    def __is_player_name_request(self,string):
+
+    def __is_player_name_request(self, string):
         message = string.split()
         return len(message) == 3 and message[0] == "player" and message[2] == "name"
 
     def __parse_player_name_request(self, string):
         message = string.split()
-        if message[1] == "north" : return self.__make_dict("player_name_request", Identifiers.NORTH)
-        if message[1] == "south" : return self.__make_dict("player_name_request", Identifiers.SOUTH)
+        if message[1] == "north":
+            return make_dict("player_name_request", Identifiers.NORTH)
+        if message[1] == "south":
+            return make_dict("player_name_request", Identifiers.SOUTH)
         raise InvalidParseError(string)
 
-    def __make_dict(self, type, value):
-        return {"type":type, "value":value}
 
+class ClientCommandParser(object):
+    """
+    Meant for parsing messages that come from the server
+    """
+    def parse(self, message):
+        """
+        Parse the mesage and return a dict of information
+        @param message the message to be parsed
+        @return a dictionary of {type: message type,  values : arbitrary}
+        @raise InvalidParseError if the message was not of the type we expected
+        """
+        raise InvalidParseError(message)
 
 class InvalidParseError(Exception):
 
--- test/battleline/engine/testCommandParser.py
+++ test/battleline/engine/testCommandParser.py
@@ -1,13 +1,17 @@
 import unittest
-from battleline.engine.CommandParser import CommandParser, InvalidParseError, Identifiers
+from battleline.engine.CommandParser import ServerCommandParser, ClientCommandParser, InvalidParseError, Identifiers
 
 
 class TestCommandParser(unittest.TestCase):
 
-    def test_invalid_message_thrown_if_invalid_message(self):
+    def test_invalid_message_thrown_if_invalid_message_server(self):
         self.assertRaisesRegexp(
-            InvalidParseError, "Invalid Parsed Message - Invalid Message", CommandParser().parse, "Invalid Message")
+            InvalidParseError, "Invalid Parsed Message - Invalid Message", ServerCommandParser().parse, "Invalid Message")
+        self.assertRaisesRegexp(
+            InvalidParseError, "Invalid Parsed Message - Invalid Message", ClientCommandParser().parse, "Invalid Message")
 
     def test_can_parse_player_name_request(self):
-        self.assertEquals({"type": "player_name_request", "value" : Identifiers.NORTH}, CommandParser().parse("player north name"))
-        self.assertEquals({"type": "player_name_request", "value" : Identifiers.SOUTH}, CommandParser().parse("player south name"))
+        self.assertEquals({"type": "player_name_request", "value": Identifiers.NORTH},
+                          ServerCommandParser().parse("player north name"))
+        self.assertEquals({"type": "player_name_request", "value": Identifiers.SOUTH},
+                          ServerCommandParser().parse("player south name"))
