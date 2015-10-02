7f70d61f96c5eebd19da0379bdfcb5dccd5b7c0f
Pat Viafore <patviafore@gmail.com>
2015-10-01 21:05:55 -0500

    Adding player name request parsing

--- battleline/engine/CommandParser.py
+++ battleline/engine/CommandParser.py
@@ -1,3 +1,8 @@
+
+class Identifiers(object):
+    NORTH = "north"
+    SOUTH = "south"
+
 class CommandParser(object):
 
     def parse(self, message):
@@ -7,10 +12,25 @@ class CommandParser(object):
         @return a dictionary of {type: message type,  values : arbitrary}
         @raise InvalidParseError if the message was not of the type we expected
         """
+        if self.__is_player_name_request(message): return self.__parse_player_name_request(message)
         raise InvalidParseError(message)
+
+    def __is_player_name_request(self,string):
+        message = string.split()
+        return len(message) == 3 and message[0] == "player" and message[2] == "name"
+
+    def __parse_player_name_request(self, string):
+        message = string.split()
+        if message[1] == "north" : return self.__make_dict("player_name_request", Identifiers.NORTH)
+        if message[1] == "south" : return self.__make_dict("player_name_request", Identifiers.SOUTH)
+        raise InvalidParseError(string)
+
+    def __make_dict(self, type, value):
+        return {"type":type, "value":value}
 
 
 class InvalidParseError(Exception):
+
     def __init__(self, message):
         """
         Constructor
@@ -22,5 +42,5 @@ class InvalidParseError(Exception):
         """
         Return a string representation
         @return a string representation
-        """W
+        """
         return "Invalid Parsed Message - {}".format(self.message)
--- test/battleline/engine/testCommandParser.py
+++ test/battleline/engine/testCommandParser.py
@@ -1,5 +1,5 @@
 import unittest
-from battleline.engine.CommandParser import CommandParser, InvalidParseError
+from battleline.engine.CommandParser import CommandParser, InvalidParseError, Identifiers
 
 
 class TestCommandParser(unittest.TestCase):
@@ -7,3 +7,7 @@ class TestCommandParser(unittest.TestCase):
     def test_invalid_message_thrown_if_invalid_message(self):
         self.assertRaisesRegexp(
             InvalidParseError, "Invalid Parsed Message - Invalid Message", CommandParser().parse, "Invalid Message")
+
+    def test_can_parse_player_name_request(self):
+        self.assertEquals({"type": "player_name_request", "value" : Identifiers.NORTH}, CommandParser().parse("player north name"))
+        self.assertEquals({"type": "player_name_request", "value" : Identifiers.SOUTH}, CommandParser().parse("player south name"))
