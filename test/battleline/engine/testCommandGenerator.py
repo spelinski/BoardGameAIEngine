47cf0ccf25cc3e647d563055064ccbf7819a85a1
Spelinski <rohk@hotmail.com>
2015-10-01 21:51:47 -0500

    after the autopep 8

--- test/battleline/engine/testCommandGenerator.py
+++ test/battleline/engine/testCommandGenerator.py
@@ -68,22 +68,25 @@ class TestCommandGenerator(unittest.TestCase):
             cardString += " " + card.color + "," + str(card.number)
         self.assertEqual(
             self.mockCommunication.messages_received.pop(), cardString)
-    
+
     def test_send_flag_claim_status_no_claims(self):
-        localCommandGenerator = CommandGenerator(self.mockCommunication, "north")
+        localCommandGenerator = CommandGenerator(
+            self.mockCommunication, "north")
         claimStatusString = "flag claim-status"
         flagList = []
-        for _ in range(1,10):
+        for _ in range(1, 10):
             claimStatusString += " unclaimed"
             flagList.append(Flag())
         localCommandGenerator.send_flag_claim_status(flagList)
-        self.assertEqual(self.mockCommunication.messages_received.pop(), claimStatusString)
-    
+        self.assertEqual(
+            self.mockCommunication.messages_received.pop(), claimStatusString)
+
     def test_send_flag_claim_status_north_claim(self):
-        localCommandGenerator = CommandGenerator(self.mockCommunication, "north")
+        localCommandGenerator = CommandGenerator(
+            self.mockCommunication, "north")
         claimStatusString = "flag claim-status"
         flagList = []
-        for _ in range(1,9):
+        for _ in range(1, 9):
             claimStatusString += " unclaimed"
             flagList.append(Flag())
         tempFlag = Flag()
@@ -91,13 +94,15 @@ class TestCommandGenerator(unittest.TestCase):
         flagList.append(tempFlag)
         claimStatusString += " north"
         localCommandGenerator.send_flag_claim_status(flagList)
-        self.assertEqual(self.mockCommunication.messages_received.pop(), claimStatusString)
-        
+        self.assertEqual(
+            self.mockCommunication.messages_received.pop(), claimStatusString)
+
     def test_send_flag_claim_status_south_claim(self):
-        localCommandGenerator = CommandGenerator(self.mockCommunication, "north")
+        localCommandGenerator = CommandGenerator(
+            self.mockCommunication, "north")
         claimStatusString = "flag claim-status"
         flagList = []
-        for _ in range(1,9):
+        for _ in range(1, 9):
             claimStatusString += " unclaimed"
             flagList.append(Flag())
         tempFlag = Flag()
@@ -105,46 +110,58 @@ class TestCommandGenerator(unittest.TestCase):
         flagList.append(tempFlag)
         claimStatusString += " south"
         localCommandGenerator.send_flag_claim_status(flagList)
-        self.assertEqual(self.mockCommunication.messages_received.pop(), claimStatusString)
-    
+        self.assertEqual(
+            self.mockCommunication.messages_received.pop(), claimStatusString)
+
     def test_send_flag_cards_no_cards(self):
-        localCommandGenerator = CommandGenerator(self.mockCommunication, "north")
+        localCommandGenerator = CommandGenerator(
+            self.mockCommunication, "north")
         flagList = []
-        for _ in range(1,10):
+        for _ in range(1, 10):
             flagList.append(Flag())
         localCommandGenerator.send_flag_cards(flagList)
         i = 1
         for _ in flagList:
             flagString = "flag " + str(i) + " cards"
-            self.assertEqual(self.mockCommunication.messages_received.pop(), flagString + " south")
-            self.assertEqual(self.mockCommunication.messages_received.pop(), flagString + " north")
+            self.assertEqual(
+                self.mockCommunication.messages_received.pop(), flagString + " south")
+            self.assertEqual(
+                self.mockCommunication.messages_received.pop(), flagString + " north")
             ++i
-            
+
     def test_send_flag_cards_one_card(self):
-        localCommandGenerator = CommandGenerator(self.mockCommunication, "north")
+        localCommandGenerator = CommandGenerator(
+            self.mockCommunication, "north")
         flagList = []
-        for _ in range(1,9):
+        for _ in range(1, 9):
             flagList.append(Flag())
         lastFlag = Flag()
-        lastFlag.add_card("Player North",TroopCard(1,"RED"))
+        lastFlag.add_card("Player North", TroopCard(1, "RED"))
         localCommandGenerator.send_flag_cards(flagList)
         i = 1
         for _ in flagList:
             flagString = "flag " + str(i) + " cards"
-            self.assertEqual(self.mockCommunication.messages_received.pop(), flagString + " south")
+            self.assertEqual(
+                self.mockCommunication.messages_received.pop(), flagString + " south")
             if i == 10:
                 flagString += " RED,1"
-                self.assertEqual(self.mockCommunication.messages_received.pop(), flagString + " north")
+                self.assertEqual(
+                    self.mockCommunication.messages_received.pop(), flagString + " north")
             else:
-                self.assertEqual(self.mockCommunication.messages_received.pop(), flagString + " north")
+                self.assertEqual(
+                    self.mockCommunication.messages_received.pop(), flagString + " north")
             ++i
-    
+
     def test_send_opponent_play(self):
-        localCommandGenerator = CommandGenerator(self.mockCommunication, "north")
-        localCommandGenerator.send_opponent_play(2, TroopCard(2,"RED"))
-        self.assertEqual(self.mockCommunication.messages_received.pop(), "opponent play 2 RED,2")
-    
+        localCommandGenerator = CommandGenerator(
+            self.mockCommunication, "north")
+        localCommandGenerator.send_opponent_play(2, TroopCard(2, "RED"))
+        self.assertEqual(
+            self.mockCommunication.messages_received.pop(), "opponent play 2 RED,2")
+
     def test_send_go_play(self):
-        localCommandGenerator = CommandGenerator(self.mockCommunication, "north")
+        localCommandGenerator = CommandGenerator(
+            self.mockCommunication, "north")
         localCommandGenerator.send_go_play()
-        self.assertEqual(self.mockCommunication.messages_received.pop(), "go play-card")
+        self.assertEqual(
+            self.mockCommunication.messages_received.pop(), "go play-card")
