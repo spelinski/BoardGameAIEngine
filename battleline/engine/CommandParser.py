1e9dec7e75209b81f2f2d9c8b3ae363b7d0825fe
Pat Viafore <patviafore@gmail.com>
2015-10-02 21:34:21 -0500

    Fixing full use cases for flag claim

--- battleline/engine/CommandParser.py
+++ battleline/engine/CommandParser.py
@@ -1,6 +1,5 @@
 from battleline.Identifiers import *
 
-
 def make_dict(type, value):
     return {"type": type, "value": value}
 
@@ -23,8 +22,7 @@ class ServerCommandParser(object):
             return self.__parse_colors_message(message)
         if self.__is_player_hand_message(message):
             return self.__parse_player_hand_message(message)
-        if self.__is_flag_claim_message(message):
-            return make_dict("flag_claim", ["unclaimed", "north", "south", "unclaimed", "north", "south", "unclaimed", "north", "south"])
+        if self.__is_flag_claim_message(message): return self.__parse_flag_claim_message(message)
         raise InvalidParseError(message)
 
     def __is_player_name_request(self, string):
@@ -47,7 +45,7 @@ class ServerCommandParser(object):
 
     def __is_player_hand_message(self, string):
         message = string.split()
-        return len(message) >= 3 and len(message) <= 10 and message[0] == "player" and Identifiers.is_player_valid(message[1]) and message[2] == "hand"
+        return len(message) >= 3 and len(message) <=10 and message[0] == "player" and Identifiers.is_player_valid(message[1]) and message[2] == "hand"
 
     def __parse_player_hand_message(self, string):
         message = string.split()
@@ -67,6 +65,9 @@ class ServerCommandParser(object):
         message = string.split()
         return len(message) == 11 and message[0] == "flag" and message[1] == "claim-status" and all(status in ["unclaimed", Identifiers.NORTH, Identifiers.SOUTH] for status in message[3:])
 
+    def __parse_flag_claim_message(self,string):
+        message = string.split()
+        return make_dict("flag_claim" , message[2:])
 
 class ClientCommandParser(object):
     """
