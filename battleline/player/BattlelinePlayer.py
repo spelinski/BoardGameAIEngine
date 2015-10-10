0c2803c7a633136f78f7f5ba5b6e6e08c1966838
Jonathan Hood <me@jonathanhood.org>
2015-10-10 09:28:05 -0500

    Add first (untested) impl of the subprocess player

--- battleline/player/BattlelinePlayer.py
+++ battleline/player/BattlelinePlayer.py
@@ -15,6 +15,43 @@ class Player(object):
         if next_card is not None:
             self.hand.append(next_card)
 
+class SubprocessPlayer(Player):
+    def __init__(self, communication):
+        self.communication = communication
+
+    def new_game(self, direction, initial_hand):
+        super(subprocessplayer, self).new_game(direction, initial_hand)
+
+        self.generator = CommandGenerator(self.communication, direction)
+        self.generator.send_player_direction_name()
+        
+        try:
+            self.name = ClientCommandParser().parse(
+                self.communication.get_response())["value"][1]
+        except:
+            self.name = direction
+        
+        self.generator.send_colors()
+
+    def compute_turn(self,board):
+        # Send current state to subprocess
+        self.generator.send_player_hand(self.hand)
+        self.generator.send_flag_claim_status(board.flags)
+        self.generator.send_flag_cards(board.flags)
+        # Send last move?
+
+        # Tell them to play
+        self.generator.send_go_play()
+
+        # Retrieve action from the subprocess
+        try:
+          data = ClientCommandParser().parse(self.communication.get_response())
+          flag, card = data["value"]
+        except InvalidParseError:
+          flag, card = 1, None
+
+        return flag, card 
+
 class BattlelinePlayer(object):
     """
     Player object for Battleline
