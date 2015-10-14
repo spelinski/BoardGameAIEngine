ea9799c64110b2099065d3bd93af7abe7d197b09
Pat Viafore <patviafore@gmail.com>
2015-10-13 22:39:00 -0500

    Updating makefile

--- Battleline.py
+++ Battleline.py
@@ -3,13 +3,21 @@ from battleline.engine.BattlelineEngine import BattlelineEngine
 from battleline.player.BattlelinePlayer import SubprocessPlayer
 from communcation.PlayerCommunication import PlayerCommunication
 
+
 def main():
-    player1_command, player2_command = sys.argv[1:3]
-    player1 = SubprocessPlayer(PlayerCommunication(player1_command))
-    player2 = SubprocessPlayer(PlayerCommunication(player2_command))
-    engine = BattlelineEngine(player1, player2)
-    engine.run_until_game_end()
-    print "PLAYER : {} HAS WON".format(engine.get_winning_player())
+    try:
+        player1_command, player2_command = sys.argv[1:3]
+        comm1 = PlayerCommunication(player1_command)
+        comm2 = PlayerCommunication(player2_command)
+        player1 = SubprocessPlayer(comm1)
+        player2 = SubprocessPlayer(comm2)
+        engine = BattlelineEngine(player1, player2)
+        engine.run_until_game_end()
+        print "PLAYER : {} HAS WON".format(engine.get_winning_player())
+    except:
+        comm1.close()
+        comm2.close()
+        raise
 
 if __name__ == "__main__":
     main()
--- Makefile
+++ Makefile
@@ -12,5 +12,7 @@ coverage:
 	coverage report -m --skip-covered
 clean-output:
 	rm output*.txt
+run-server:
+	python Battleline.py "python runStarterBot.py" "python runStarterBot.py"
 test: unit_test acceptance_test
 all: format test coverage
