382e6d9522793b0097e9bd1b47c82018161de03f
Pat Viafore <patviafore@gmail.com>
2015-10-13 20:34:59 -0500

    Adding failing battleline engine test

---
+++ test/acceptance/BattlelineEngineAcceptanceTest.py
@@ -0,0 +1,8 @@
+import unittest
+from battleline.engine.BattlelineEngine  import BattlelineEngine
+class BattlelineEngineAcceptanceTest(unittest.TestCase):
+
+
+    def test_can_run_complete_game(self):
+        engine = BattlelineEngine(None, None)
+        self.fail()
