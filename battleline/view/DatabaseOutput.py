b385a5f66fccfa689a5a27e2afecf61a323e540a
Spelinski <rohk@hotmail.com>
2015-10-26 22:30:47 -0500

    pep8

--- battleline/view/DatabaseOutput.py
+++ battleline/view/DatabaseOutput.py
@@ -13,56 +13,62 @@ class DatabaseOutput:
     def __init__(self):
         client = pymongo.MongoClient('localhost', 27017)
         db = client['test_database']
-        initPost = {"northPlayerName" : "",
-                    "southPlayerName" : "",
-                    "actionsTaken" : [],
-                    "winner" : "",
-                    "date" : datetime.datetime.utcnow()}
+        initPost = {"northPlayerName": "",
+                    "southPlayerName": "",
+                    "actionsTaken": [],
+                    "winner": "",
+                    "date": datetime.datetime.utcnow()}
         self.posts = db.posts
         self.post_id = self.posts.insert_one(initPost).inserted_id
-        self.playerNames = {Identifiers.NORTH:'player1', Identifiers.SOUTH:'player2'}
-
+        self.playerNames = {Identifiers.NORTH: 'player1',
+                            Identifiers.SOUTH: 'player2'}
 
     def setup_player_positions(self, playerName, place):
         self.playerNames[place] = playerName
         if place == "north":
-            self.posts.update({'_id':self.post_id}, {"northPlayerName":playerName})
+            self.posts.update({'_id': self.post_id}, {
+                              "northPlayerName": playerName})
         else:
-            self.posts.update({'_id':self.post_id}, {"southPlayerName":playerName})
-            
+            self.posts.update({'_id': self.post_id}, {
+                              "southPlayerName": playerName})
+
     def get_north_player_name(self):
-        whatever = self.posts.find({'_id':self.post_id})
+        whatever = self.posts.find({'_id': self.post_id})
         myDocument = whatever.next()
         return myDocument['northPlayerName']
-    
+
     def get_south_player_name(self):
-        whatever = self.posts.find({'_id':self.post_id})
+        whatever = self.posts.find({'_id': self.post_id})
         myDocument = whatever.next()
         return myDocument['southPlayerName']
-        
+
     def draw_action(self, place, card):
-        myOutput = "{} draws {} {}".format(self.playerNames[place], str(card.number), card.color)
-        self.posts.update({'_id':self.post_id}, { '$push': {"actionsTaken": myOutput}})
-    
+        myOutput = "{} draws {} {}".format(
+            self.playerNames[place], str(card.number), card.color)
+        self.posts.update({'_id': self.post_id}, {
+                          '$push': {"actionsTaken": myOutput}})
+
     def play_action(self, place, card, flagNumber):
-        myOutput = "{} plays {} {} {}".format(self.playerNames[place], str(card.number), card.color, str(flagNumber))
-        self.posts.update({'_id':self.post_id}, { '$push': {"actionsTaken": myOutput}})
-        
+        myOutput = "{} plays {} {} {}".format(
+            self.playerNames[place], str(card.number), card.color, str(flagNumber))
+        self.posts.update({'_id': self.post_id}, {
+                          '$push': {"actionsTaken": myOutput}})
+
     def claim_action(self, place, flagNumber):
         myOutput = self.playerNames[place] + " claims " + str(flagNumber)
-        self.posts.update({'_id':self.post_id}, { '$push': {"actionsTaken": myOutput}})
-        
+        self.posts.update({'_id': self.post_id}, {
+                          '$push': {"actionsTaken": myOutput}})
+
     def get_action(self):
-        whatever = self.posts.find({'_id':self.post_id})
+        whatever = self.posts.find({'_id': self.post_id})
         myDocument = whatever.next()
         return str(myDocument['actionsTaken'][0])
-    
+
     def declare_winner(self, place):
         myOutput = self.playerNames[place] + " wins"
-        self.posts.update({'_id':self.post_id},{"winner":myOutput})
-        
+        self.posts.update({'_id': self.post_id}, {"winner": myOutput})
+
     def get_winner(self):
-        whatever = self.posts.find({'_id':self.post_id})
+        whatever = self.posts.find({'_id': self.post_id})
         myDocument = whatever.next()
         return myDocument['winner']
-        
