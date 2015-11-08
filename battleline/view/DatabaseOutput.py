81cf944a7623876427d2ee0fd06ec564012817f3
Spelinski <rohk@hotmail.com>
2015-11-07 23:01:35 -0600

    get down to just what's needed before trying to mock

--- battleline/view/DatabaseOutput.py
+++ battleline/view/DatabaseOutput.py
@@ -2,73 +2,53 @@ import pymongo
 import datetime
 from battleline.Identifiers import Identifiers
 
-'''import os.path
-from battleline.Identifiers import Identifiers
-
-ACTIONS = ['draw', 'play', 'claim', 'win']'''
-
-
 class DatabaseOutput:
 
-    def __init__(self):
-        client = pymongo.MongoClient('localhost', 27017)
-        db = client['test_database']
+    def __init__(self, host, port, database_name):
+        self.client = pymongo.MongoClient(host, port)
+        db = self.client[database_name]
+        if not "games" in db.collection_names(include_system_collections=False):
+            db.create_collection("games", capped=True, size = 100000000, max = 10000)
         initPost = {"northPlayerName": "",
                     "southPlayerName": "",
                     "actionsTaken": [],
                     "winner": "",
                     "date": datetime.datetime.utcnow()}
-        self.posts = db.posts
-        self.post_id = self.posts.insert_one(initPost).inserted_id
+            
+        self.games = db.games
+        self.post_id = self.games.insert_one(initPost).inserted_id
         self.playerNames = {Identifiers.NORTH: 'player1',
                             Identifiers.SOUTH: 'player2'}
+        
+    def delete_database(self, database_name):
+        self.client.drop_database(database_name)
 
     def setup_player_positions(self, playerName, place):
         self.playerNames[place] = playerName
         if place == "north":
-            self.posts.update({'_id': self.post_id}, {
+            self.games.update({'_id': self.post_id}, {
                               "northPlayerName": playerName})
         else:
-            self.posts.update({'_id': self.post_id}, {
+            self.games.update({'_id': self.post_id}, {
                               "southPlayerName": playerName})
-
-    def get_north_player_name(self):
-        whatever = self.posts.find({'_id': self.post_id})
-        myDocument = whatever.next()
-        return myDocument['northPlayerName']
-
-    def get_south_player_name(self):
-        whatever = self.posts.find({'_id': self.post_id})
-        myDocument = whatever.next()
-        return myDocument['southPlayerName']
 
     def draw_action(self, place, card):
         myOutput = "{} draws {} {}".format(
             self.playerNames[place], str(card.number), card.color)
-        self.posts.update({'_id': self.post_id}, {
+        self.games.update({'_id': self.post_id}, {
                           '$push': {"actionsTaken": myOutput}})
 
     def play_action(self, place, card, flagNumber):
         myOutput = "{} plays {} {} {}".format(
             self.playerNames[place], str(card.number), card.color, str(flagNumber))
-        self.posts.update({'_id': self.post_id}, {
+        self.games.update({'_id': self.post_id}, {
                           '$push': {"actionsTaken": myOutput}})
 
     def claim_action(self, place, flagNumber):
         myOutput = self.playerNames[place] + " claims " + str(flagNumber)
-        self.posts.update({'_id': self.post_id}, {
+        self.games.update({'_id': self.post_id}, {
                           '$push': {"actionsTaken": myOutput}})
-
-    def get_action(self):
-        whatever = self.posts.find({'_id': self.post_id})
-        myDocument = whatever.next()
-        return str(myDocument['actionsTaken'][0])
 
     def declare_winner(self, place):
         myOutput = self.playerNames[place] + " wins"
-        self.posts.update({'_id': self.post_id}, {"winner": myOutput})
-
-    def get_winner(self):
-        whatever = self.posts.find({'_id': self.post_id})
-        myDocument = whatever.next()
-        return myDocument['winner']
+        self.games.update({'_id': self.post_id}, {"winner": myOutput})
--- test/battleline/view/testDatabaseOutput.py
+++ test/battleline/view/testDatabaseOutput.py
@@ -6,41 +6,41 @@ from battleline.Identifiers import Identifiers, TroopCard
 class TestDatabaseOutput(unittest.TestCase):
 
     def setUp(self):
-        self.output = DatabaseOutput()
+        self.output = DatabaseOutput('localhost', 27017, 'test_database')
 
-    '''def tearDown(self):
-        os.remove(self.output.filename)'''
+    def tearDown(self):
+        self.output.delete_database('test_database')
 
     def test_setup_player_positions_north(self):
         self.output.setup_player_positions('Hooch', Identifiers.NORTH)
-        myOutput = self.output.get_north_player_name()
+        myOutput = self.output.games.find({'_id': self.output.post_id}).next()['northPlayerName']
         self.assertEqual(myOutput, 'Hooch')
 
     def test_setup_player_positions_south(self):
         self.output.setup_player_positions('Pooch', Identifiers.SOUTH)
-        myOutput = self.output.get_south_player_name()
+        myOutput = self.output.games.find({'_id': self.output.post_id}).next()['southPlayerName']
         self.assertEqual(myOutput, 'Pooch')
 
     def test_drawing_card_north(self):
         self.output.setup_player_positions('Synergy', Identifiers.NORTH)
         self.output.draw_action(Identifiers.NORTH, TroopCard(1, "red"))
-        myOutput = self.output.get_action()
+        myOutput =  self.output.games.find({'_id': self.output.post_id}).next()['actionsTaken'][0]
         self.assertEqual(myOutput, "Synergy draws 1 red")
 
     def test_playing_card_north_flag_one(self):
         self.output.setup_player_positions('Synergy', Identifiers.NORTH)
         self.output.play_action(Identifiers.NORTH, TroopCard(1, "red"), 1)
-        myOutput = self.output.get_action()
+        myOutput = self.output.games.find({'_id': self.output.post_id}).next()['actionsTaken'][0]
         self.assertEqual(myOutput, "Synergy plays 1 red 1")
 
     def test_claiming_flag_one_north(self):
         self.output.setup_player_positions('Synergy', Identifiers.NORTH)
         self.output.claim_action(Identifiers.NORTH, 1)
-        myOutput = self.output.get_action()
+        myOutput = self.output.games.find({'_id': self.output.post_id}).next()['actionsTaken'][0]
         self.assertEqual(myOutput, "Synergy claims 1")
 
     def test_north_wins(self):
         self.output.setup_player_positions('Synergy', Identifiers.NORTH)
         self.output.declare_winner(Identifiers.NORTH)
-        myOutput = self.output.get_winner()
+        myOutput = self.output.games.find({'_id': self.output.post_id}).next()['winner']
         self.assertEqual(myOutput, "Synergy wins")
