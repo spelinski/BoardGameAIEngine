ab9d0c899e607d9fbda2afc807bfb0529a4b2b21
Spelinski <rohk@hotmail.com>
2015-11-08 20:52:15 -0600

    mock out pymongo

--- battleline/view/DatabaseOutput.py
+++ battleline/view/DatabaseOutput.py
@@ -2,24 +2,26 @@ import pymongo
 import datetime
 from battleline.Identifiers import Identifiers
 
+
 class DatabaseOutput:
 
     def __init__(self, host, port, database_name):
         self.client = pymongo.MongoClient(host, port)
         db = self.client[database_name]
         if not "games" in db.collection_names(include_system_collections=False):
-            db.create_collection("games", capped=True, size = 100000000, max = 10000)
+            db.create_collection("games", capped=True,
+                                 size=100000000, max=10000)
         initPost = {"northPlayerName": "",
                     "southPlayerName": "",
                     "actionsTaken": [],
                     "winner": "",
                     "date": datetime.datetime.utcnow()}
-            
+
         self.games = db.games
         self.post_id = self.games.insert_one(initPost).inserted_id
         self.playerNames = {Identifiers.NORTH: 'player1',
                             Identifiers.SOUTH: 'player2'}
-        
+
     def delete_database(self, database_name):
         self.client.drop_database(database_name)
 
---
+++ test/battleline/view/MockPyMongo.py
@@ -0,0 +1,51 @@
+class MockMongoClient(object):
+    def __init__(self, host, port):
+        pass
+        
+    def __getitem__(self, database_name):
+        return MockDatabase(database_name)
+    
+    def drop_database(self, database_name):
+        pass
+        
+class MockDatabase(object):
+    def __init__(self, database_name):
+        self.name = database_name
+        self.collection_name_list = [""]
+        self.games = MockGameCollection()
+    
+    def collection_names(self, include_system_collections):
+        return self.collection_name_list
+    
+    def create_collection(self, collection_name, capped=False, size=0, max=0):
+        self.collection_name_list = [collection_name]
+
+class MockGameCollection(object):
+    def __init__(self):
+        self.my_GameEntry = ""
+    
+    def insert_one(self, entry_info):
+        self.my_GameEntry = MockGameEntry(entry_info)
+        return self.my_GameEntry
+    
+    def update(self, id_json, update_json):
+        self.my_GameEntry.update(update_json)
+    
+    def find(self, id_json):
+        return self.my_GameEntry
+    
+class MockGameEntry(object):
+    def __init__(self, init_json):
+        self.inserted_id = 1
+        self.entry_info = init_json
+    
+    def next(self):
+        return self.entry_info
+    
+    def update(self, update_json):
+        for name in update_json:
+            if name == "$push":
+                for realName in update_json[name]:
+                    self.entry_info[realName] = [update_json[name][realName]]
+            else:
+                self.entry_info[name] = update_json[name]
