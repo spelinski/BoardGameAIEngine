8af994af51d407095a769c5189e8a3aae9e0f6e1
Pat Viafore <patviafore@gmail.com>
2016-08-11 22:06:11 -0500

    Adding ability to take cards

--- test/dominion/model/test_supply.py
+++ test/dominion/model/test_supply.py
@@ -6,50 +6,52 @@ from dominion.model.Supply import *
 class TestSupply(unittest.TestCase):
 
     def check_first_set_kingdom_and_treasure_cards(self, supply):
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Cellar"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Market"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Militia"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Mine"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Moat"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Remodel"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Smithy"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Village"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Woodcutter"))
-        self.assertEquals(10, supply.get_number_of_kingdom_cards("Workshop"))
+        self.assertEquals(10, supply.get_number_of_cards("Cellar"))
+        self.assertEquals(10, supply.get_number_of_cards("Market"))
+        self.assertEquals(10, supply.get_number_of_cards("Militia"))
+        self.assertEquals(10, supply.get_number_of_cards("Mine"))
+        self.assertEquals(10, supply.get_number_of_cards("Moat"))
+        self.assertEquals(10, supply.get_number_of_cards("Remodel"))
+        self.assertEquals(10, supply.get_number_of_cards("Smithy"))
+        self.assertEquals(10, supply.get_number_of_cards("Village"))
+        self.assertEquals(10, supply.get_number_of_cards("Woodcutter"))
+        self.assertEquals(10, supply.get_number_of_cards("Workshop"))
 
-        self.assertEquals(60, supply.get_number_of_treasure_cards("Copper"))
-        self.assertEquals(40, supply.get_number_of_treasure_cards("Silver"))
-        self.assertEquals(30, supply.get_number_of_treasure_cards("Gold"))
+        self.assertEquals(60, supply.get_number_of_cards("Copper"))
+        self.assertEquals(40, supply.get_number_of_cards("Silver"))
+        self.assertEquals(30, supply.get_number_of_cards("Gold"))
 
     def test_first_set_two_players(self):
         supply = Supply(2, "First Game")
         self.check_first_set_kingdom_and_treasure_cards(supply)
 
-        self.assertEquals(14, supply.get_number_of_victory_cards("Estate"))
-        self.assertEquals(8, supply.get_number_of_victory_cards("Duchy"))
-        self.assertEquals(8, supply.get_number_of_victory_cards("Province"))
+        self.assertEquals(14, supply.get_number_of_cards("Estate"))
+        self.assertEquals(8, supply.get_number_of_cards("Duchy"))
+        self.assertEquals(8, supply.get_number_of_cards("Province"))
 
-        self.assertEquals(10, supply.get_number_of_curse_cards())
+        self.assertEquals(10, supply.get_number_of_cards("Curse"))
 
     def test_first_set_three_players(self):
         supply = Supply(3, "First Game")
         self.check_first_set_kingdom_and_treasure_cards(supply)
 
-        self.assertEquals(21, supply.get_number_of_victory_cards("Estate"))
-        self.assertEquals(12, supply.get_number_of_victory_cards("Duchy"))
-        self.assertEquals(12, supply.get_number_of_victory_cards("Province"))
+        self.assertEquals(21, supply.get_number_of_cards("Estate"))
+        self.assertEquals(12, supply.get_number_of_cards("Duchy"))
+        self.assertEquals(12, supply.get_number_of_cards("Province"))
 
-        self.assertEquals(20, supply.get_number_of_curse_cards())
+        self.assertEquals(20, supply.get_number_of_cards("Curse"))
 
     def test_first_set_three_players(self):
         supply = Supply(4, "First Game")
         self.check_first_set_kingdom_and_treasure_cards(supply)
 
-        self.assertEquals(24, supply.get_number_of_victory_cards("Estate"))
-        self.assertEquals(12, supply.get_number_of_victory_cards("Duchy"))
-        self.assertEquals(12, supply.get_number_of_victory_cards("Province"))
+        self.assertEquals(24, supply.get_number_of_cards("Estate"))
+        self.assertEquals(12, supply.get_number_of_cards("Duchy"))
+        self.assertEquals(12, supply.get_number_of_cards("Province"))
 
-        self.assertEquals(30, supply.get_number_of_curse_cards())
+        self.assertEquals(30, supply.get_number_of_cards("Curse"))
 
-    def take_card_decreases_supply_appropriately(self):
+    def test_take_card_decreases_supply_appropriately(self):
         supply = Supply(2, "First Game")
+        supply.take("Province")
+        self.assertEquals(7, supply.get_number_of_cards("Province"))
