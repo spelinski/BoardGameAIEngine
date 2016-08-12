0b5fb672d329671515f7c32877dd2b07c4db9df0
Pat Viafore <patviafore@gmail.com>
2016-08-11 22:06:11 -0500

    Adding ability to take cards

--- dominion/model/Supply.py
+++ dominion/model/Supply.py
@@ -1,31 +1,40 @@
 class Supply(object):
     def __init__(self, number_of_players, set):
-        self.supply = self.create_supply(number_of_players, set)
+        self.supply = self.__create_supply(number_of_players, set)
 
-    def create_supply(self, number_of_players, set):
+    def __create_supply(self, number_of_players, set):
         kingdom_cards = ["Cellar", "Market", "Militia", "Mine", "Moat", "Remodel", "Smithy", "Village", "Woodcutter", "Workshop"]
-        return {"kingdom": {card:10 for card in kingdom_cards},
-                "treasure": {"Copper": 60, "Silver" : 40, "Gold" : 30},
-                "victory": {card:_get_victory_card_count(card, number_of_players) for card in ["Estate", "Duchy", "Province"]},
-                "curse" : _get_curse_card_count(number_of_players)
-                }
+        other_supply_cards = ["Copper", "Silver", "Gold", "Estate", "Duchy", "Province", "Curse"]
+        cards = kingdom_cards + other_supply_cards
+        return  {card: self.__get_initial_number_of_cards(card, number_of_players) for card in cards}
 
-    def get_number_of_kingdom_cards(self, card):
-        return self.supply["kingdom"][card]
+    def __get_initial_number_of_cards(self, card, number_of_players):
+        if self.__is_victory_card(card):
+            return self.__get_victory_card_count(card, number_of_players)
+        elif card == "Curse":
+            return self.__get_curse_card_count(number_of_players)
+        elif card == "Copper":
+            return 60
+        elif card == "Silver":
+            return 40
+        elif card == "Gold":
+            return 30
+        else:
+            return 10
 
-    def get_number_of_treasure_cards(self, card):
-        return self.supply["treasure"][card]
+    def __is_victory_card(self,card):
+        return card in ["Estate", "Duchy", "Province"]
 
-    def get_number_of_victory_cards(self, card):
-        return self.supply["victory"][card]
+    def __get_victory_card_count(self, card, number_of_players):
+        victory_cards =  8 if number_of_players == 2 else 12
+        victory_cards += (3*number_of_players) if card == "Estate" else 0
+        return victory_cards
 
-    def get_number_of_curse_cards(self):
-        return self.supply["curse"]
+    def __get_curse_card_count(self, number_of_players):
+        return (number_of_players - 1) * 10
 
-def _get_victory_card_count(card, number_of_players):
-    victory_cards =  8 if number_of_players == 2 else 12
-    victory_cards += (3*number_of_players) if card == "Estate" else 0
-    return victory_cards
+    def get_number_of_cards(self, card):
+        return self.supply[card]
 
-def _get_curse_card_count(number_of_players):
-    return (number_of_players - 1) * 10
+    def take(self, card):
+        self.supply[card] = self.supply[card] - 1
