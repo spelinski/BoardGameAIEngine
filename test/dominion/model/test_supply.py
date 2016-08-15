80b520f78dc668e93e7945a651204101f6f3dec9
Pat Viafore <patviafore@gmail.com>
2016-08-14 21:07:32 -0500

    Adding game set identifiers

--- test/dominion/model/test_supply.py
+++ test/dominion/model/test_supply.py
@@ -22,7 +22,7 @@ class TestSupply(unittest.TestCase):
         self.assertEquals(30, supply.get_number_of_cards(Identifiers.GOLD))
 
     def test_first_set_two_players(self):
-        supply = Supply(2, "First Game")
+        supply = Supply(2, Identifiers.FIRST_GAME)
         self.check_first_set_kingdom_and_treasure_cards(supply)
 
         self.assertEquals(14, supply.get_number_of_cards(Identifiers.ESTATE))
@@ -32,7 +32,7 @@ class TestSupply(unittest.TestCase):
         self.assertEquals(10, supply.get_number_of_cards(Identifiers.CURSE))
 
     def test_first_set_three_players(self):
-        supply = Supply(3, "First Game")
+        supply = Supply(3, Identifiers.FIRST_GAME)
         self.check_first_set_kingdom_and_treasure_cards(supply)
 
         self.assertEquals(21, supply.get_number_of_cards(Identifiers.ESTATE))
@@ -41,8 +41,8 @@ class TestSupply(unittest.TestCase):
 
         self.assertEquals(20, supply.get_number_of_cards(Identifiers.CURSE))
 
-    def test_first_set_three_players(self):
-        supply = Supply(4, "First Game")
+    def test_first_set_four_players(self):
+        supply = Supply(4, Identifiers.FIRST_GAME)
         self.check_first_set_kingdom_and_treasure_cards(supply)
 
         self.assertEquals(24, supply.get_number_of_cards(Identifiers.ESTATE))
@@ -52,28 +52,28 @@ class TestSupply(unittest.TestCase):
         self.assertEquals(30, supply.get_number_of_cards(Identifiers.CURSE))
 
     def test_take_card_decreases_supply_appropriately(self):
-        supply = Supply(2, "First Game")
+        supply = Supply(2, Identifiers.FIRST_GAME)
         supply.take(Identifiers.PROVINCE)
         self.assertEquals(7, supply.get_number_of_cards(Identifiers.PROVINCE))
 
     def test_cant_take_card_not_in_supply(self):
-        supply = Supply(2, "First Game")
+        supply = Supply(2, Identifiers.FIRST_GAME)
         with self.assertRaises(CardNotInSupplyException):
             supply.take(Identifiers.FEAST)
 
     def test_cant_take_card_not_if_pile_is_empty(self):
-        supply = Supply(2, "First Game")
+        supply = Supply(2, Identifiers.FIRST_GAME)
         for _ in range(8):
             supply.take(Identifiers.DUCHY)
         with self.assertRaises(PileEmptyException):
             supply.take(Identifiers.DUCHY)
 
     def test_no_piiles_are_empty_to_start(self):
-        supply = Supply(2, "First Game")
+        supply = Supply(2, Identifiers.FIRST_GAME)
         self.assertEquals(0, supply.get_number_of_empty_piles())
 
     def test_can_empty_piles(self):
-        supply = Supply(2, "First Game")
+        supply = Supply(2, Identifiers.FIRST_GAME)
         for _ in range(10):
             supply.take(Identifiers.MOAT)
             supply.take(Identifiers.REMODEL)
