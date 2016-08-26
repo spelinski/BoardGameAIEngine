a57513b5c1af542537f2dccb349f29f5e070f786
Pat Viafore <patviafore@gmail.com>
2016-08-25 22:16:20 -0500

    Adding get_cost

--- dominion/CardInfo.py
+++ dominion/CardInfo.py
@@ -2,3 +2,25 @@ from dominion import Identifiers
 
 def is_victory_card(card):
     return card in [Identifiers.ESTATE, Identifiers.DUCHY, Identifiers.PROVINCE]
+
+def get_victory_points(card):
+    if card == Identifiers.ESTATE: return 1
+    if card == Identifiers.DUCHY: return 3
+    if card == Identifiers.PROVINCE: return 6
+    if card == Identifiers.CURSE: return -1
+    return 0
+
+def get_worth(card):
+    if card == Identifiers.COPPER: return 1
+    if card == Identifiers.SILVER: return 2
+    if card == Identifiers.GOLD: return 3
+    return 0
+
+def get_cost(card):
+    if card in [Identifiers.CURSE, Identifiers.COPPER]: return 0
+    if card in [Identifiers.MOAT, Identifiers.CELLAR, Identifiers.ESTATE]: return 2
+    if card in [Identifiers.SILVER, Identifiers.VILLAGE, Identifiers.WOODCUTTER, Identifiers.WORKSHOP] : return 3
+    if card in [Identifiers.REMODEL, Identifiers.SMITHY, Identifiers.MILITIA] : return 4
+    if card in [Identifiers.MINE, Identifiers.MARKET, Identifiers.DUCHY] : return 5
+    if card in [Identifiers.GOLD]: return 6
+    if card in [Identifiers.PROVINCE] : return 8
--- test/dominion/test_card_info.py
+++ test/dominion/test_card_info.py
@@ -5,3 +5,38 @@ class TestCardInfo(unittest.TestCase):
 
     def test_can_check_if_victory_card(self):
         self.assertTrue(is_victory_card(Identifiers.ESTATE))
+        self.assertTrue(is_victory_card(Identifiers.DUCHY))
+        self.assertTrue(is_victory_card(Identifiers.PROVINCE))
+        self.assertFalse(is_victory_card(Identifiers.CURSE))
+
+    def test_can_victory_points(self):
+        self.assertEquals(1, get_victory_points(Identifiers.ESTATE))
+        self.assertEquals(3, get_victory_points(Identifiers.DUCHY))
+        self.assertEquals(6, get_victory_points(Identifiers.PROVINCE))
+        self.assertEquals(-1, get_victory_points(Identifiers.CURSE))
+        self.assertEquals(0, get_victory_points(Identifiers.COPPER))
+
+    def test_can_get_worth(self):
+        self.assertEquals(1, get_worth(Identifiers.COPPER))
+        self.assertEquals(2, get_worth(Identifiers.SILVER))
+        self.assertEquals(3, get_worth(Identifiers.GOLD))
+        self.assertEquals(0, get_worth(Identifiers.MOAT))
+
+    def test_can_get_cost(self):
+        self.assertEquals(0, get_cost(Identifiers.CURSE))
+        self.assertEquals(0, get_cost(Identifiers.COPPER))
+        self.assertEquals(2, get_cost(Identifiers.CELLAR))
+        self.assertEquals(2, get_cost(Identifiers.ESTATE))
+        self.assertEquals(2, get_cost(Identifiers.MOAT))
+        self.assertEquals(3, get_cost(Identifiers.SILVER))
+        self.assertEquals(3, get_cost(Identifiers.VILLAGE))
+        self.assertEquals(3, get_cost(Identifiers.WOODCUTTER))
+        self.assertEquals(3, get_cost(Identifiers.WORKSHOP))
+        self.assertEquals(4, get_cost(Identifiers.MILITIA))
+        self.assertEquals(4, get_cost(Identifiers.REMODEL))
+        self.assertEquals(4, get_cost(Identifiers.SMITHY))
+        self.assertEquals(5, get_cost(Identifiers.MARKET))
+        self.assertEquals(5, get_cost(Identifiers.MINE))
+        self.assertEquals(5, get_cost(Identifiers.DUCHY))
+        self.assertEquals(6, get_cost(Identifiers.GOLD))
+        self.assertEquals(8, get_cost(Identifiers.PROVINCE))
