ada4a7561ea864d94361a32e70d230fdcab5fcba
Pat Viafore <patviafore@gmail.com>
2016-08-26 21:34:08 -0500

    adding invalid case

--- dominion/communication/CommunicationFlow.py
+++ dominion/communication/CommunicationFlow.py
@@ -3,5 +3,9 @@ import json
 
 def send_player_info(player, player_number, version):
     player_info_request = CommandGenerator().create_player_info_request(player_number, version)
-    response = player.send_message_and_await_response(json.dumps(player_info_request))
-    player.name = response["name"]
+    json_response = player.send_message_and_await_response(json.dumps(player_info_request))
+    try:
+        response = json.loads(json_response)
+        player.name = response["name"]
+    except:
+        raise Exception("Message was not JSON: {}".format(json_response))
--- test/dominion/communication/test_communication_flow.py
+++ test/dominion/communication/test_communication_flow.py
@@ -4,20 +4,33 @@ import json
 
 from dominion.communication.CommunicationFlow import *
 
+def create_player(send_and_wait_func):
+    player = Mock()
+    player.send_message_and_await_response = send_and_wait_func
+    return player
+
 class TestCommunicationFlow(unittest.TestCase):
 
     def test_can_handle_player_request(self):
-        player = Mock()
+
         def send_message_and_await_response( json_message):
             message = json.loads(json_message)
             self.assertEquals("player-name-request", message["type"])
-            return {
+            return json.dumps({
                      "type": "player-name-reply",
                      "player_number": message["player_number"],
                      "name": "test-bot",
                      "version": message["version"]
-                    }
+                    })
 
-        player.send_message_and_await_response = send_message_and_await_response
+        player = create_player(send_message_and_await_response)
         send_player_info(player, 1, 1)
         self.assertEquals("test-bot", player.name)
+
+    def test_player_request_aborts_if_not_json(self):
+        def invalid_message( json_message):
+            return "nope"
+
+        player = create_player(invalid_message)
+        with self.assertRaisesRegexp(Exception, "Message was not JSON: nope"):
+            send_player_info(player, 1, 1)
