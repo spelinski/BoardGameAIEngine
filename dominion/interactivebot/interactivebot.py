c0e1fc6945a91a75b2bf2522f75d5f363c30f10d
Pat Viafore <patviafore@gmail.com>
2016-09-07 20:01:45 -0500

    Fixing weird copy paste problem

--- dominion/interactivebot/interactivebot.py
+++ dominion/interactivebot/interactivebot.py
@@ -62,7 +62,7 @@ class InteractiveBot(object):
                     trash = raw_input()
                     self.print_message("Pick desired card")
                     desired = raw_input()
-                    self.message = {"type": "play-reply", "phasee said the directions so fast that the group couldn’t get": "action", "card": card, "additional_parameters": {"card_to_trash":trash, "desired_card":desired}}
+                    self.message = {"type": "play-reply", "phase": "action", "card": card, "additional_parameters": {"card_to_trash":trash, "desired_card":desired}}
                 if card == CELLAR:
                     self.print_message( "CELLAR")
                     self.print_message("Pick cards to discard {}".format(hand))
