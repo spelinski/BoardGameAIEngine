707244b690c3e7d7694eeb10a6894e3ac333e509
Jonathan Densford <drofsned@gmail.com>
2016-09-22 21:00:32 -0500

    adding remaining player messages to eventlistener

--- dominion/events/EventListener.py
+++ dominion/events/EventListener.py
@@ -9,3 +9,11 @@ class EventListener(object):
             broadcast_message(self.players, CommandGenerator().create_player_shuffled_message(self.number))
         if notification.type == "gained-cards":
         	broadcast_message(self.players, CommandGenerator().create_player_gained_message(self.number, notification.cards))
+        if notification.type == "played-cards":
+        	broadcast_message(self.players, CommandGenerator().create_player_played_message(self.number, notification.cards))
+        if notification.type == "trashed-cards":
+        	broadcast_message(self.players, CommandGenerator().create_player_trashed_message(self.number, notification.cards))
+        if notification.type == "discard-card":
+        	broadcast_message(self.players, CommandGenerator().create_player_discard_message(self.number, notification.card))
+        if notification.type == "revealed-cards":
+        	broadcast_message(self.players, CommandGenerator().create_player_reveal_message(self.number, notification.cards))
--- test/dominion/events/testEventListener.py
+++ test/dominion/events/testEventListener.py
@@ -28,6 +28,26 @@ class TestEventListener(unittest.TestCase):
         for player in self.players:
             self.assertEquals({"type": "player-gained", "player_number": 1, "gained": range(3)}, player.received_message)
 
+    def test_can_respond_to_played(self):
+        self.listener.notify(Notification("played-cards", cards=range(4)))
+        for player in self.players:
+            self.assertEquals({"type": "player-played", "player_number": 1, "played": range(4)}, player.received_message)
+
+    def test_can_respond_to_trashed(self):
+        self.listener.notify(Notification("trashed-cards", cards=range(2)))
+        for player in self.players:
+            self.assertEquals({"type": "player-trashed", "player_number": 1, "trashed": range(2)}, player.received_message)
+
+    def test_can_respond_to_discard(self):
+        self.listener.notify(Notification("discard-card", card="ace-of-spades"))
+        for player in self.players:
+            self.assertEquals({"type": "player-top-discard", "player_number": 1, "card": "ace-of-spades"}, player.received_message)
+
+    def test_can_respond_to_revealed(self):
+        self.listener.notify(Notification("revealed-cards", cards=range(2)))
+        for player in self.players:
+            self.assertEquals({"type": "player-reveal", "player_number": 1, "cards": range(2)}, player.received_message)
+
     def test_wrong_notification_doesnt_get_sent(self):
         self.listener.notify(Notification(""))
         for player in self.players:
